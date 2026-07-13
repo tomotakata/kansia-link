3744e6edf29e2b17b42892c53ecc27f12da77919
tomotakata <otomo.palco.me@gmail.com>
2026-07-13 18:09:17 +0900

    fix: ビルド失敗の原因だったjsx-a11yエラーを解消(Enter暗黙送信防止を本番反映)

--- components/companies/CompanyForm.tsx
+++ components/companies/CompanyForm.tsx
@@ -297,6 +297,7 @@ export default function CompanyForm({ company }: CompanyFormProps) {
   }
 
   return (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
     <form
       onSubmit={(e) => e.preventDefault()}
       onKeyDown={(e) => {
